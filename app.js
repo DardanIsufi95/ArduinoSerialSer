91da5f454b0477d2d0882152d1de9b76f7aa19e7
DardanIsufi95 <36553576+DardanIsufi95@users.noreply.github.com>
2019-05-23 02:14:13 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app).listen(3000 , function(){
+    console.log('Express server listening on port 3000!');
+});
+var io = require('socket.io').listen(server);
+
+//Format 
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+
+
+io.sockets.on('connection', function (socket) {
+//   socket.on('sensors', function (data) {
+//     socket.emit('data', 'Im Node');
+//   });
+
+    socket.on('WTN', function (data) {
+        io.sockets.emit("NTC" , data);
+    });
+
+    socket.on('CTN', function (data) {
+        buzz = data;
+        io.sockets.emit("NTW" , data);
+    });
+
+});
+
