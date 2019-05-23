19624143af2b425bc3281126f9e2ac7dadafdf7c
DardanIsufi95 <36553576+DardanIsufi95@users.noreply.github.com>
2019-05-23 14:25:23 +0200

    Update driver.py

--- driver.py
+++ driver.py
@@ -1,18 +1,52 @@
 import serial
 import serial.tools.list_ports
 import time
+import threading
+import socketio
 
-arduino_ports = [
-    p.device
-    for p in serial.tools.list_ports.comports()
-    if 'Arduino' in p.description
-]
 
-if len(arduino_ports) > 1:
-    warnings.warn('Multiple Arduinos found - using the first')
+def connect():
+	arduino_ports = [
+		p.device
+		for p in serial.tools.list_ports.comports()
+		if 'Arduino' in p.description
+	]
 
-if not arduino_ports:
-    print("No Arduino found")
-    
+	if len(arduino_ports) > 1:
+		print('Multiple Arduinos found - using the first')
+
+	if not arduino_ports:
+		print("No Arduino found")
+		time.sleep(1.5)
+		connect()
+
 ser = serial.Serial(arduino_ports[0])
 ser.baudrate = 9200
+
+sio = socketio.Client()
+sio.connect('http://test-dardan-test.1d35.starter-us-east-1.openshiftapps.com')	
+
+l = ""
+
+@sio.on('NTC')
+def on_NTC(data):
+	d = string.strip(str(data))
+    if(d != l)
+		l = d
+		ser.write(d.encode())
+		
+		
+
+		
+		
+		
+def send_data()
+
+def ReadSerial(ser):
+	while True:
+		reading = ser.readline().decode()
+		send_data(reading)
+
+
+thread = threading.Thread(target=ReadSerial, args=(ser))
+thread.start()
